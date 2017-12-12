a31a474bce1ce63b8ddded0f057545af746c3335
Jan Stanley Go <stango97@gmail.com>
2017-12-11 23:35:09 -0500

    Push

---
+++ src/main/java/ca/goldenphoenicks/lockerauto/databases/Products.java
@@ -0,0 +1,11 @@
+package ca.goldenphoenicks.lockerauto.databases;
+
+/**
+ * Created by Stanley on 12/11/17.
+ */
+
+public class Products {
+    public int id;
+    public String name;
+    public int status;
+}
