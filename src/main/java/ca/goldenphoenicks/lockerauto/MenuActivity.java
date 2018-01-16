0e72863ebd7eb50cb30e33ad9fcd39fb7e15ae09
YohaanA <31858928+YohaanA@users.noreply.github.com>
2018-01-15 23:30:28 -0500

    Add files via upload

--- src/main/java/ca/goldenphoenicks/lockerauto/MenuActivity.java
+++ src/main/java/ca/goldenphoenicks/lockerauto/MenuActivity.java
@@ -5,11 +5,14 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -19,6 +22,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import static android.widget.Toast.LENGTH_SHORT;
 
 public class MenuActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -26,14 +40,59 @@ public class MenuActivity extends AppCompatActivity
     SharedPreferences pref;
     SharedPreferences.Editor editor;
 
+    String username;
+
     public static Menu menuNav;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        MenuItem nav_item1 = (MenuActivity.menuNav).findItem(R.id.nav_lock);
+        MenuItem nav_item2 = (MenuActivity.menuNav).findItem(R.id.nav_door);
+        MenuItem nav_item3 = (MenuActivity.menuNav).findItem(R.id.nav_display);
+        nav_item1.setEnabled(false);
+        nav_item2.setEnabled(false);
+        nav_item3.setEnabled(false);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        String res="";
+        try
+        {
+            res = new MenuActivity.GetPin().execute().get();
+            if(res.contains("Success"))
+            {
+                Toast.makeText(MenuActivity.this, "Retrieved Data <3", LENGTH_SHORT).show();
+            }
+            else
+            {
+                Toast.makeText(MenuActivity.this, "Failed to Load Data", LENGTH_SHORT).show();
+            }
+
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
+
+        String[] arrayOfString = res.split("\\s+");
+        for(int i=0;i<arrayOfString.length;i++)
+        {
+            if(arrayOfString[i]== "1")
+            {
+                nav_item1.setEnabled(true);
+            }
+            if(arrayOfString[i]== "2")
+            {
+                nav_item2.setEnabled(true);
+            }
+            if(arrayOfString[i]== "3")
+            {
+                nav_item3.setEnabled(true);
+            }
+        }
+
 // Consider making QR Scanner a fab
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
@@ -76,26 +135,19 @@ public class MenuActivity extends AppCompatActivity
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
+        Intent i = getIntent();
+        username=i.getStringExtra("name");
+
+        if(username!= null) {
+            editor.putString("u_name", username);
+            editor.commit();
+        }
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        View headerV = navigationView.getHeaderView(0);
+        TextView navTitle = (TextView)headerV.findViewById(R.id.headerName);
+        navTitle.setText("Welcome, " + pref.getString("u_name",""));
         navigationView.setNavigationItemSelectedListener(this);
         navigationView.getMenu().getItem(0).setChecked(true);
-
-        // for testing whether user needs to register device or not
-        Intent carried = getIntent();
-        int over=carried.getIntExtra("validate",0);
-        menuNav = navigationView.getMenu();
-        if(over==2) {
-            MenuItem nav_item2 = menuNav.findItem(R.id.nav_lock);
-            nav_item2.setEnabled(false);
-            MenuItem nav_item3 = menuNav.findItem(R.id.nav_door);
-            nav_item3.setEnabled(false);
-            MenuItem nav_item4 = menuNav.findItem(R.id.nav_display);
-            nav_item4.setEnabled(false);
-        }
-        else{
-            MenuItem nav_item4 = menuNav.findItem(R.id.nav_qr_scanner);
-            nav_item4.setEnabled(false);
-        }
 
 //        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
 //            Window window = getWindow();
@@ -120,7 +172,7 @@ public class MenuActivity extends AppCompatActivity
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer =  findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -183,4 +235,38 @@ public class MenuActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+
+
+    class GetPin extends AsyncTask<String,Void,String> {
+
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            String result = "";
+            HttpURLConnection urlConnection = null;
+            String validateUrl = "http://munro.humber.ca/~n01116269/returnproducts.php?u_name=" + pref.getString("u_name", "");
+            try {
+                URL url = new URL(validateUrl);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream is = new BufferedInputStream(urlConnection.getInputStream());
+                BufferedReader br = new BufferedReader(new InputStreamReader(is));
+                String line = "";
+
+                while((line = br.readLine())!=null)
+                {
+                    result+=line;
+                }
+                is.close();
+                Log.i("Result",result);
+                return result;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+    }
+
 }
